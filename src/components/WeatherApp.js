420e5666906bd4702fd4de5898cc16bce83a4a7b
Juan Cortes <alias99002@outlook.com>
2023-01-11 10:32:54 -0500

    Deploy

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,60 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { WeatherForm } from "./WeatherForm";
 import { WeatherMainInfo } from "./WeatherMainInfo";
-import styles from '../styles/WeatherApp.module.css';
+import styles from "../styles/WeatherApp.module.css";
 import { Loading } from "./Loading";
 
-
-
-
 function WeatherApp() {
-    const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState(null);
 
-    useEffect(()=> {
-        loadInfo();
-    }, []);
-    useEffect(()=> {
-        document.title = `Weather | ${weather?.response.profile.tz ?? ""}`
-    }, [weather]);
-    async function loadInfo(city = "paris,fr") {
-        // const options = {
-        //     method: 'GET',
-        //     headers: {
-        //         'X-RapidAPI-Key': `${}`,
-        //         'X-RapidAPI-Host': `${}`
-        //     }
-        // };
-        try{
-            const request = await fetch(`${process.env.REACT_APP_URL}/${city}`,
-            {
-                method: 'GET',
-                headers: {
-                    
-                    'X-RapidAPI-Key': process.env.REACT_APP_KEY,
-                    'X-RapidAPI-Host': process.env.REACT_APP_URL_HOST
-                }
-            })
-            const json = await request.json()
-            
-            // console.log(json);
-            setWeather(json);
-        } catch(error){
-            console.error(error)
-        }
-    }
-   //this function calls onCityChage in thge component WeatherForm
-    function handleChangeCity(city) {
-        setWeather(null);
-        loadInfo(city);
-    }
+  async function loadInfo(city = "paris,fr") {
+    try {
+      const request = await fetch(`${process.env.REACT_APP_URL}/${city}`, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_URL_HOST,
+        },
+      });
+      const json = await request.json();
 
-    return(
-        <div className={styles.weatherContainer}>
-            <WeatherForm onChangeCity={handleChangeCity}/>
-            { weather ? <WeatherMainInfo weather={weather} /> : <Loading />}
-            
-        </div>
-    );
+      // console.log(json);
+      setWeather(json);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    loadInfo();
+  }, []);
+
+  useEffect(() => {
+    document.title = `Weather | ${weather?.response.profile.tz ?? ""}`;
+  }, [weather]);
+
+  //this function calls onCityChage in thge component WeatherForm
+  function handleChangeCity(city) {
+    setWeather(null);
+    loadInfo(city);
+  }
+
+  return (
+    <div className={styles.weatherContainer}>
+      <WeatherForm onChangeCity={handleChangeCity} />
+      {weather ? <WeatherMainInfo weather={weather} /> : <Loading />}
+    </div>
+  );
 }
 
 export { WeatherApp };
